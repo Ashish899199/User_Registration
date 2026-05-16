151dcf92fe14657af3eb1c3734701e7c923ab664
Ashishs <shrivastavaashish8991@gmail.com>
2026-05-16 16:17:53 +0530

    Add print statement

--- src/main/java/org/example/UserController.java
+++ src/main/java/org/example/UserController.java
@@ -12,6 +12,7 @@ public class UserController {
     private final UserService userService;
 
     public UserController(UserService userService) {
+        System.out.println("Add new changes ");
         this.userService = userService;
     }
 
